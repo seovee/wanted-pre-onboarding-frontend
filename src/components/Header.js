a4ea624f887bff5358579001464419eaa00367fa
seovee <jinseob102@naver.com>
2023-08-12 17:25:11 +0900

    초기세팅 / React-Router 세팅

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <h1>
+      <ul>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link to={"/singin"}>Sign In</Link>
+        </li>
+      </ul>
+    </h1>
+  );
+}
+
+export default Header;

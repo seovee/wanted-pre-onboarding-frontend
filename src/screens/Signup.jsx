f86e002fdabd201e47f4d32495c7c617e22af650
seovee <jinseob102@naver.com>
2023-08-13 14:37:06 +0900

    Signup페이지 CSS 수정

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,23 +1,61 @@
+import { styled } from "styled-components";
+import Container from "../components/Container";
+
+const SignupTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+  margin: 50px;
+`;
+
+const SignupForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 30vw;
+`;
+
+const SignupInput = styled.input`
+  height: 40px;
+  border: 1px solid darkgrey;
+  padding: 0 10px;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const SignButton = styled.button`
+  border-radius: 10px;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function Signup() {
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form>
-        <input
+    <Container>
+      <SignupTitle>회원가입</SignupTitle>
+      <SignupForm>
+        <SignupInput
           type="email"
           aria-label="이메일"
           name="email"
-          placeholder="email@naver.com을 입력해 주세요."
+          placeholder="이메일을 입력해 주세요."
         />
-        <input
+        <SignupInput
           type="password"
           aria-label="비밀번호"
           name="password"
           placeholder="비밀번호를 입력해주세요"
         />
-        {true ? <button>회원가입</button> : <button disabled>회원가입</button>}
-      </form>
-    </>
+        {true ? (
+          <SignButton>회원가입</SignButton>
+        ) : (
+          <SignButton disabled style={{ cursor: "not-allowed" }}>
+            회원가입
+          </SignButton>
+        )}
+      </SignupForm>
+    </Container>
   );
 }
 

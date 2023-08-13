bc04d03d51fa17296b205815404cdbfac1ab2b3c
seovee <jinseob102@naver.com>
2023-08-13 14:04:33 +0900

    Header 컴포넌트 styled 추가, 배치 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+
+const NavContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const NavList = styled.li`
+  list-style: none;
+  font-size: 35px;
+  font-weight: 700;
+`;
+
+const NavListLink = styled(NavLink)`
+  text-decoration: none;
+  color: orange;
+`;
 
 function Header() {
+  const activeStyle = {
+    color: "blue",
+  };
   return (
-    <h1>
-      <ul>
-        <li>
-          <Link to={"/"}>Home</Link>
-        </li>
-        <li>
-          <Link to={"/signup"}>Signup</Link>
-        </li>
-        <li>
-          <Link to={"/signin"}>Sign In</Link>
-        </li>
-        <li>
-          <Link to={"/todo"}>ToDo</Link>
-        </li>
-      </ul>
-    </h1>
+    <>
+      <h1>Assignment</h1>
+      <NavContainer>
+        <NavList>
+          <NavListLink
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            to={"/"}
+          >
+            Home
+          </NavListLink>
+        </NavList>
+        <NavList>
+          <NavListLink
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            to={"/signup"}
+          >
+            Signup
+          </NavListLink>
+        </NavList>
+        <NavList>
+          <NavListLink
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            to={"/signin"}
+          >
+            Sign In
+          </NavListLink>
+        </NavList>
+        <NavList>
+          <NavListLink
+            style={({ isActive }) => (isActive ? activeStyle : {})}
+            to={"/todo"}
+          >
+            ToDo
+          </NavListLink>
+        </NavList>
+      </NavContainer>
+    </>
   );
 }
 
